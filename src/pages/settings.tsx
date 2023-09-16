4af9840be830b88cba47da67917a7077a9609c04
Alexis Pineda <ixspnd.ap@gmail.com>
2023-09-16 10:43:27 +0800

    feat(app): initial settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,37 @@
+import ContentHeader from '@/components/global/ContentHeader'
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemContent,
+  Typography
+} from '@mui/joy'
 import React from 'react'
 
 const SettingsPage = () => {
   return (
-    <div>SettingsPage</div>
+    <>
+      <ContentHeader
+        header="Settings"
+      />
+      <Container>
+        <List>
+          <Typography level="body-xs">Theme</Typography>
+          <ListItem>
+            Dark Mode
+          </ListItem>
+          <Typography level="body-xs">Account</Typography>
+          <ListItem>
+            Change password
+          </ListItem>
+          <ListItem>
+            Logout
+          </ListItem>
+        </List>
+      </Container>
+    </>
   )
 }
 
