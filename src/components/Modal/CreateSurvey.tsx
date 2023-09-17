bc8017f013358e546b5ce341c045d1b1f13d5231
Alexis Pineda <ixspnd.ap@gmail.com>
2023-09-17 14:11:13 +0800

    feat(app): survey forms in tabs

--- src/components/Modal/CreateSurvey.tsx
+++ src/components/Modal/CreateSurvey.tsx
@@ -1,10 +1,5 @@
 import { styled } from '@/stitches.config'
 import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardContent,
-  Divider,
   FormControl,
   FormHelperText,
   FormLabel,
@@ -12,26 +7,19 @@ import {
   Input,
   Modal,
   ModalClose,
-  ModalDialog,
-  Option,
-  Select,
-  Switch, 
-  Sheet,
-  Tab,
+  ModalDialog, Tab,
   TabList,
   TabPanel,
   Tabs,
   Typography
 } from '@mui/joy'
 import {
-  Trash as TrashIcon
-} from 'lucide-react'
-import {
   FC,
   useEffect,
   useState
 } from 'react'
 import { v4 as uuid } from 'uuid'
+import FieldsTab from './components/Tabs/FieldsTab'
 
 type CreateSurveyModalProps = {
   isOpen: boolean
@@ -56,13 +44,13 @@ const CreateSurveyModal: FC<CreateSurveyModalProps> = ({
       type: 'text',
       defaultValue: '',
       options: [],
-      isFullScreen: false
+      isFullScreen: true
     }
     // TODO: add logic to check if there are names existing
     setFields(fields.concat(defaultData))
   }
 
-  const removeField = (id: number) => {
+  const removeField = (id: string) => {
     setFields(prevState => prevState.filter((v:any) => v?.id !== id))
   }
   
@@ -103,86 +91,28 @@ const CreateSurveyModal: FC<CreateSurveyModalProps> = ({
           </Grid>
 
           <Grid md={6}>
-            <FieldWrapper>
-              <Grid 
-                container 
-                spacing={1}
-                direction='column'
-              > 
-                {fields.map((field, index) => {
-                  return <Grid  key={index} xs={12}>
-                    <Card>
-                      <CardContent 
-                        orientation='horizontal'
-                        sx={{
-                          display: 'flex',
-                          justifyContent: 'space-between',
-                          alignItems: 'center'
-                        }}
-                      >
-                        <Typography level="title-sm">Field {index + 1}</Typography>
-                        <ButtonGroup>
-                          <Button 
-                            size="sm"
-                            color="danger"
-                            variant="soft"
-                            startDecorator={<TrashIcon/>}
-                            onClick={() => removeField(field?.id)}
-                          >Remove</Button>
-                        </ButtonGroup>
-                      </CardContent>
-                      <Divider inset='context'/>
-                      <CardContent orientation="horizontal">
-                        <FormControl error={false}>
-                          <FormLabel>Form name</FormLabel>
-                          <Input />
-                        </FormControl>
-                        <Divider orientation="vertical"/>
-                        <FormControl error={false}>
-                          <FormLabel>Form Label</FormLabel>
-                          <Input />
-                        </FormControl>
-                      </CardContent>
-                      <Divider/>
-                      <CardContent>
-                        <FormControl error={false}>
-                          <FormLabel>Type</FormLabel>
-                          <Select defaultValue="text">
-                            <Option value="text">Text</Option>
-                            <Option value="email">Email</Option>
-                            <Option value="number">Number</Option>
-                            <Option value="checkbox">Checkbox</Option>
-                            <Option value="radio">Radio</Option>
-                          </Select>
-                        </FormControl>
-                        <FormControl>
-                          <Typography startDecorator={
-                            <Switch/>
-                          }>Fullscreen</Typography>
-                        </FormControl>
-                      </CardContent>
-                      {/* <Divider />
-                      <CardContent>
-                        <Typography>asdf</Typography>
-                      </CardContent> */}
-                    </Card>
-                  </Grid>
-                })}
 
-                <Grid xs={12}>
-                  <Button 
-                    fullWidth
-                    variant="soft"
-                    color="neutral"
-                    onClick={addField}
-                  >
-                    Add {fields.length > 1 && 'another'} field
-                  </Button>
-                </Grid>
-
-              </Grid>
-
-            </FieldWrapper>
+            <Tabs>
+              <TabList>
+                <Tab value="fields">Fields</Tab>
+                <Tab value="logic">Logic</Tab>
+                <Tab value="theme">Theme</Tab>
+                <Tab value="options">Options</Tab>
+              </TabList>
+              <FieldWrapper>
+                <TabPanel value="fields">
+                  <FieldsTab
+                    fields={fields}
+                    onHandleRemove={removeField}
+                    onHandleAdd={addField}
+                  />
+                </TabPanel>
+                <TabPanel value="logic">Logic</TabPanel>
+                <TabPanel value="theme">Theme</TabPanel>
+                <TabPanel value="options">Options</TabPanel>
+              </FieldWrapper>
+            </Tabs>
+            
 
           </Grid>
           <Grid md={6}>asdf</Grid>
