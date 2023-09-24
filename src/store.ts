e9758e9b75f20bd6312d1e7641790517dd465ee2
Alexis Pineda <ixspnd.ap@gmail.com>
2023-09-24 20:35:31 +0800

    fix(survey): store - fix issue in not rendering survey forms by default

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { SAMPLE_FORMS } from './helpers/constants'
 
-export const useSurveyStore = () => create(set => ({
+export const useSurveyStore = create(set => ({
   surveys: SAMPLE_FORMS,
   selectedSurvey: {},
   setSelectedSurvey: (data) => set(() => ({selectedSurvey: data})),
