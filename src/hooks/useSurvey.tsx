a1503432669751e1fc22cf486a6699cebd737397
Alexis Pineda <ixspnd.ap@gmail.com>
2023-10-14 09:49:26 +0800

    chore: remove zustand

--- src/hooks/useSurvey.tsx
+++ src/hooks/useSurvey.tsx
@@ -1,27 +1,47 @@
-import { useSurveyStore } from '@/store'
 import { Survey } from '@/types/survey'
 import { useEffect, useState } from 'react'
-import { shallow } from 'zustand/shallow'
+import useApi from './useApi'
 
 const SURVEY_STORAGE = 'user-surveys'
 
 const useSurvey = () => {
+
   const {
-    surveys,
-    selectedSurvey: selectedSurveyStore, 
-    clearSelectedSurvey,
-    setSelectedSurvey,
-    setSurveys
-  } = useSurveyStore((draft) => ({
-    surveys: draft.surveys,
-    selectedSurvey: draft.selectedSurvey,
-    clearSelectedSurvey: draft.clearSelectedSurvey,
-    setSelectedSurvey: draft.setSelectedSurvey,
-    setSurveys: draft.setSurveys
-  }), shallow)
+    data: createdSurvey,
+    isLoading: isCreatingSurvey,
+    fetch: createSurvey,
+    error: createSurveyError,
+  } = useApi('createSurvey')
+
+  const {
+    data: userSurveys,
+    isLoading: isLoadingUserSurveys,
+    fetch: fetchUserSurveys,
+    error,
+  } = useApi('getSurveysByUser')
+
+  const [surveys, setSurveys] = useState([])
+  const [selectedSurvey, setSelectedSurvey] = useState({})
+
+  const clearSelectedSurvey = () => {
+    setSelectedSurvey({})
+  }
 
   const storeSurveys = (data) => {
     localStorage.setItem(SURVEY_STORAGE, JSON.stringify(data))
+  }
+
+  const getUserSurveys = async (userId) => {
+    try {
+      await fetchUserSurveys({
+        params: {
+          userId
+        }
+      }) 
+      storeSurveys(userSurveys)
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   const selectSurvey = (survey?: Survey) => {
@@ -34,25 +54,32 @@ const useSurvey = () => {
   
   const saveSurvey = (data:Survey) => {
     if (data) {
+      createSurvey({data})
       setSurveys(surveys.concat(data))
     }
   }
 
   const removeSurvey = (index: number) => {
-    setSurveys(surveys.filter((v,i) => i !== index))
+    setSurveys(surveys?.filter((v,i) => i !== index))
   }
 
+  /** 
+   * Stores user surveys to the state
+   */
   useEffect(()=>{
-    storeSurveys(surveys)
-  }, [surveys])
+    setSurveys(userSurveys)
+    storeSurveys(userSurveys)
+  }, [userSurveys])
 
   return ({
+    isCreatingSurvey,
     surveys, 
+    getUserSurveys,
     selectSurvey,
     storeSurveys, 
     saveSurvey,
     removeSurvey,
-    selectedSurvey: selectedSurveyStore, 
+    selectedSurvey, 
   })
 }
 
